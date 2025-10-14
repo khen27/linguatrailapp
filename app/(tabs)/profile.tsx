beb1490a24c45ba294b1533ddaa8b5d60fe34be5
Karl Henderson <karl@braqs.co>
2025-10-14 16:22:41 +0200

    Update profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, View, Image, Text, TouchableOpacity, Switch } from 'react-native';
+import { StyleSheet, ScrollView, View, Image, Text, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useState } from 'react';
 import Svg, { Path } from 'react-native-svg';
@@ -58,7 +58,10 @@ export default function ProfileScreen() {
             </View>
             <Text style={styles.menuItemText}>Learning Configurations</Text>
           </View>
-          <MaterialIcons name="chevron-right" size={20} color="#2F4291" />
+          <Svg width={20} height={20} viewBox="0 0 20 20" fill="none">
+            <Path d="M12.0247 15.0584L17.083 10L12.0247 4.94168" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <Path d="M2.91641 10L16.9414 10" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          </Svg>
         </TouchableOpacity>
         
         {/* Subscription & Plans Menu Item */}
@@ -74,7 +77,10 @@ export default function ProfileScreen() {
             </View>
             <Text style={styles.menuItemText}>Subscription & Plans</Text>
           </View>
-          <MaterialIcons name="chevron-right" size={20} color="#2F4291" />
+          <Svg width={20} height={20} viewBox="0 0 20 20" fill="none">
+            <Path d="M12.0247 15.0584L17.083 10L12.0247 4.94168" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <Path d="M2.91641 10L16.9414 10" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          </Svg>
         </TouchableOpacity>
         
         {/* Support Menu Item */}
@@ -91,7 +97,10 @@ export default function ProfileScreen() {
             </View>
             <Text style={styles.menuItemText}>Support</Text>
           </View>
-          <MaterialIcons name="chevron-right" size={20} color="#2F4291" />
+          <Svg width={20} height={20} viewBox="0 0 20 20" fill="none">
+            <Path d="M12.0247 15.0584L17.083 10L12.0247 4.94168" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <Path d="M2.91641 10L16.9414 10" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          </Svg>
         </TouchableOpacity>
         
         {/* Privacy Policy Menu Item */}
@@ -100,15 +109,17 @@ export default function ProfileScreen() {
             <View style={styles.menuIconContainer}>
               <Svg width={40} height={40} viewBox="0 0 40 40" fill="none">
                 <Path d="M0 20C0 8.95431 8.95431 0 20 0C31.0457 0 40 8.95431 40 20C40 31.0457 31.0457 40 20 40C8.95431 40 0 31.0457 0 20Z" fill="#E9FDF8"/>
-                <Path d="M28.4248 16.625C30.4582 18.6583 30.4582 21.9583 28.4248 23.9917C26.3915 26.025 23.0915 26.025 21.0582 23.9917C19.0248 21.9583 19.0248 18.6583 21.0582 16.625C23.0915 14.5917 26.3915 14.5917 28.4248 16.625Z" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                <Path d="M21.6165 28.0333C19.9498 27.3667 18.4915 26.1583 17.5248 24.4833C16.5748 22.8417 16.2582 21.0167 16.4832 19.275" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                <Path d="M19.6165 13.7333C21.0332 12.625 22.8082 11.9667 24.7415 11.9667C26.6332 11.9667 28.3748 12.6083 29.7748 13.675" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                <Path d="M27.8665 28.0333C29.5332 27.3667 30.9915 26.1583 31.9582 24.4833C32.9082 22.8417 33.2248 21.0167 32.9998 19.275" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <Path d="M18.7418 11.8583L14.5835 13.425C13.6251 13.7833 12.8418 14.9166 12.8418 15.9333V22.125C12.8418 23.1083 13.4918 24.4 14.2835 24.9916L17.8668 27.6666C19.0418 28.55 20.9751 28.55 22.1501 27.6666L25.7335 24.9916C26.5251 24.4 27.1751 23.1083 27.1751 22.125V15.9333C27.1751 14.9083 26.3918 13.775 25.4335 13.4166L21.2751 11.8583C20.5668 11.6 19.4335 11.6 18.7418 11.8583Z" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <Path d="M20.0002 20.4166C20.9206 20.4166 21.6668 19.6704 21.6668 18.7499C21.6668 17.8294 20.9206 17.0833 20.0002 17.0833C19.0797 17.0833 18.3335 17.8294 18.3335 18.7499C18.3335 19.6704 19.0797 20.4166 20.0002 20.4166Z" stroke="#1DB289" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                <Path d="M20 20.4167V22.9167" stroke="#1DB289" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
               </Svg>
             </View>
             <Text style={styles.menuItemText}>Privacy Policy</Text>
           </View>
-          <MaterialIcons name="chevron-right" size={20} color="#2F4291" />
+          <Svg width={20} height={20} viewBox="0 0 20 20" fill="none">
+            <Path d="M12.0247 15.0584L17.083 10L12.0247 4.94168" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <Path d="M2.91641 10L16.9414 10" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          </Svg>
         </TouchableOpacity>
         
         {/* Terms & Conditions Menu Item */}
@@ -117,12 +128,18 @@ export default function ProfileScreen() {
             <View style={styles.menuIconContainer}>
               <Svg width={40} height={40} viewBox="0 0 40 40" fill="none">
                 <Path d="M0 20C0 8.95431 8.95431 0 20 0C31.0457 0 40 8.95431 40 20C40 31.0457 31.0457 40 20 40C8.95431 40 0 31.0457 0 20Z" fill="#E9FDF8"/>
-                <Path d="M15.4 17.3555L12.2266 20L15.4 22.6445C15.4649 22.6965 15.5188 22.7608 15.5585 22.8338C15.5982 22.9068 15.6229 22.987 15.6313 23.0697C15.6396 23.1524 15.6314 23.2359 15.607 23.3154C15.5827 23.3949 15.5428 23.4687 15.4896 23.5325C15.4364 23.5964 15.371 23.649 15.2972 23.6872C15.2234 23.7255 15.1428 23.7487 15.0599 23.7554C14.9771 23.7621 14.8938 23.7523 14.8148 23.7264C14.7358 23.7005 14.6628 23.6591 14.6 23.6047L10.85 20.4797C10.7796 20.421 10.723 20.3476 10.6842 20.2647C10.6453 20.1817 10.6252 20.0912 10.6252 19.9996C10.6252 19.908 10.6453 19.8175 10.6842 19.7346C10.723 19.6516 10.7796 19.5782 10.85 19.5195L14.6 16.3945C14.7274 16.2884 14.8918 16.2373 15.0569 16.2524C15.222 16.2675 15.3744 16.3476 15.4805 16.475C15.5866 16.6024 15.6377 16.7668 15.6226 16.9319C15.6075 17.097 15.5274 17.2494 15.4 17.3555ZM29.15 19.5195L25.4 16.3945C25.3369 16.342 25.2641 16.3024 25.1857 16.278C25.1073 16.2536 25.0249 16.2449 24.9431 16.2524C24.8613 16.2599 24.7819 16.2834 24.7092 16.3216C24.6365 16.3598 24.5721 16.4119 24.5195 16.475C24.4134 16.6024 24.3623 16.7668 24.3774 16.9319C24.3925 17.097 24.4726 17.2494 24.6 17.3555L27.7734 20L24.6 22.6445C24.5351 22.6965 24.4813 22.7608 24.4415 22.8338C24.4018 22.9068 24.3771 22.987 24.3688 23.0697C24.3604 23.1524 24.3686 23.2359 24.393 23.3154C24.4173 23.3949 24.4572 23.4687 24.5104 23.5325C24.5636 23.5964 24.629 23.649 24.7028 23.6872C24.7766 23.7255 24.8572 23.7487 24.9401 23.7554C25.0229 23.7621 25.1062 23.7523 25.1852 23.7264C25.2642 23.7005 25.3372 23.6591 25.4 23.6047L29.15 20.4797C29.2204 20.421 29.277 20.3476 29.3158 20.2647C29.3547 20.1817 29.3748 20.0912 29.3748 19.9996C29.3748 19.908 29.3547 19.8175 29.3158 19.7346C29.277 19.6516 29.2204 19.5782 29.15 19.5195ZM22.7133 12.5375C22.6361 12.5095 22.5542 12.497 22.4722 12.5006C22.3902 12.5042 22.3097 12.524 22.2354 12.5588C22.161 12.5935 22.0942 12.6426 22.0388 12.7031C21.9834 12.7637 21.9405 12.8346 21.9125 12.9117L16.9125 26.6617C16.8844 26.7389 16.8717 26.8209 16.8753 26.903C16.8789 26.9851 16.8987 27.0656 16.9334 27.1401C16.9682 27.2145 17.0173 27.2814 17.0779 27.3369C17.1385 27.3923 17.2095 27.4353 17.2867 27.4633C17.3552 27.4876 17.4273 27.5 17.5 27.5C17.6284 27.5 17.7536 27.4605 17.8587 27.3868C17.9638 27.3132 18.0437 27.2089 18.0875 27.0883L23.0875 13.3383C23.1155 13.2611 23.1281 13.1792 23.1244 13.0972C23.1208 13.0152 23.101 12.9347 23.0662 12.8604C23.0315 12.786 22.9824 12.7192 22.9219 12.6638C22.8613 12.6084 22.7904 12.5655 22.7133 12.5375Z" stroke="#1DB289" strokeWidth="1.5"/>
+                <Path d="M28.3332 18.3334V22.5001C28.3332 26.6667 26.6665 28.3334 22.4998 28.3334H17.4998C13.3332 28.3334 11.6665 26.6667 11.6665 22.5001V17.5001C11.6665 13.3334 13.3332 11.6667 17.4998 11.6667H21.6665" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <Path d="M28.3332 18.3334H24.9998C22.4998 18.3334 21.6665 17.5001 21.6665 15.0001V11.6667L28.3332 18.3334Z" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <Path d="M15.8335 20.8333H20.8335" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <Path d="M15.8335 24.1667H19.1668" stroke="#1DB289" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
               </Svg>
             </View>
             <Text style={styles.menuItemText}>Terms & Conditions</Text>
           </View>
-          <MaterialIcons name="chevron-right" size={20} color="#2F4291" />
+          <Svg width={20} height={20} viewBox="0 0 20 20" fill="none">
+            <Path d="M12.0247 15.0584L17.083 10L12.0247 4.94168" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <Path d="M2.91641 10L16.9414 10" stroke="#2F4291" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          </Svg>
         </TouchableOpacity>
         </View>
 
@@ -141,12 +158,12 @@ export default function ProfileScreen() {
             </View>
             <Text style={styles.preferenceItemText}>Dark theme</Text>
           </View>
-          <Switch
-            value={darkTheme}
-            onValueChange={setDarkTheme}
-            trackColor={{ false: '#F6F7FA', true: '#27EDB7' }}
-            thumbColor={darkTheme ? '#263574' : '#5C5C5C'}
-          />
+          <TouchableOpacity onPress={() => setDarkTheme(!darkTheme)}>
+            <Svg width={40} height={24} viewBox="0 0 40 24" fill="none">
+              <Path d="M0 12C0 5.37258 5.37258 0 12 0H28C34.6274 0 40 5.37258 40 12C40 18.6274 34.6274 24 28 24H12C5.37258 24 0 18.6274 0 12Z" fill={darkTheme ? "#27EDB7" : "#F6F7FA"}/>
+              <Path d={darkTheme ? "M18 12C18 6.47715 22.4772 2 28 2C33.5228 2 38 6.47715 38 12C38 17.5228 33.5228 22 28 22C22.4772 22 18 17.5228 18 12Z" : "M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12Z"} fill={darkTheme ? "#263574" : "#5C5C5C"}/>
+            </Svg>
+          </TouchableOpacity>
         </View>
         
         {/* Push Notifications Preference */}
@@ -162,12 +179,12 @@ export default function ProfileScreen() {
             </View>
             <Text style={styles.preferenceItemText}>Push notifications</Text>
           </View>
-          <Switch
-            value={pushNotifications}
-            onValueChange={setPushNotifications}
-            trackColor={{ false: '#F6F7FA', true: '#27EDB7' }}
-            thumbColor={pushNotifications ? '#263574' : '#5C5C5C'}
-          />
+          <TouchableOpacity onPress={() => setPushNotifications(!pushNotifications)}>
+            <Svg width={40} height={24} viewBox="0 0 40 24" fill="none">
+              <Path d="M0 12C0 5.37258 5.37258 0 12 0H28C34.6274 0 40 5.37258 40 12C40 18.6274 34.6274 24 28 24H12C5.37258 24 0 18.6274 0 12Z" fill={pushNotifications ? "#27EDB7" : "#F6F7FA"}/>
+              <Path d={pushNotifications ? "M18 12C18 6.47715 22.4772 2 28 2C33.5228 2 38 6.47715 38 12C38 17.5228 33.5228 22 28 22C22.4772 22 18 17.5228 18 12Z" : "M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12Z"} fill={pushNotifications ? "#263574" : "#5C5C5C"}/>
+            </Svg>
+          </TouchableOpacity>
         </View>
         
         </View>
