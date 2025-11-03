6ef6054c6795cd88d7b6127902bee571484ab9b2
Karl Henderson <karl@braqs.co>
2025-11-03 09:43:00 +0100

    lots of changes!

--- app/voice-assistant.tsx
+++ app/voice-assistant.tsx
@@ -192,7 +192,7 @@ export default function VoiceAssistantScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#202020',
+    backgroundColor: '#000000',
   },
   safeArea: {
     flex: 1,
